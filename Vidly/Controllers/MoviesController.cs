5951ec62cb66a7e3b3c16637507a88fc5679b8c8
tomOkila <thomasokilalast@gmail.com>
2019-05-04 15:09:00 +0300

    Vidly First commit fr Movies and Customers Functinality

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+
+
+            var movies = GetMovies();
+            /*var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1" },
+                new Customer {Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };*/
+
+            //Passing data to the view
+            //ViewData["Movie"] = movie;
+
+            return View(movies);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id =" + id);
+        }
+
+        public ActionResult Index1(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0} & sortBy={1}",pageIndex,sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+"/"+ month);
+        }
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1,Name="Shrek" },
+                new Movie {Id=2,Name="Wall-e" }
+            };
+        }
+    }
+}
