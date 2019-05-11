1b961f8ca3258c524d6fc048b4eb4eac1d0b903b
tomOkila <thomasokilalast@gmail.com>
2019-05-11 22:05:29 +0300

    Movie Listing and Details Display

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,71 +11,54 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public MoviesController()
         {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
+        }
 
 
-            var movies = GetMovies();
-            /*var movie = new Movie() { Name = "Shrek!" };
-
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "Customer 1" },
-                new Customer {Name = "Customer 2" }
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
-            };*/
-
-            //Passing data to the view
-            //ViewData["Movie"] = movie;
-
-            return View(movies);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-        }
-
-
-        public ActionResult Edit(int id)
-        {
-            return Content("Id =" + id);
-        }
-
-        public ActionResult Index1(int? pageIndex,string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageIndex={0} & sortBy={1}",pageIndex,sortBy));
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year+"/"+ month);
-        }
-
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie {Id=1,Name="Shrek" },
-                new Movie {Id=2,Name="Wall-e" }
             };
+
+            return View(viewModel);
         }
     }
 }
