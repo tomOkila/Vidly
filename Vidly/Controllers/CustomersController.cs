6acf7083aedf630325c766041cc902997ece71b1
tomOkila <thomasokilalast@gmail.com>
2019-08-09 10:22:35 +0300

    Customer Upload Details for Datatables CRUD Functionality and Bootbox Plugins

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,8 +26,8 @@ namespace Vidly.Controllers
         public ActionResult Index()
         {
             // var customers = GetCustomers();
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-            return View(customers);
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View();
         }
 
         /*public ActionResult Details(int id)
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+
+        public int Id { get; set; }
+
+
+        [StringLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+        public DateTime DateAdded { get; set; }
+
+       
+        public DateTime ReleaseDate { get; set; }
+
+       
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
